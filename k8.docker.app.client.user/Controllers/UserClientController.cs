0c85bdd684f4c9324874425c54cae6462b013fb9
Asif Khan <khanasif1@gmail.com>
2019-08-27 15:15:09 +1000

    Initial change

---
+++ k8.docker.app.client.user/Controllers/UserClientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace k8.docker.app.client.user.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserClientController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "!!!!!Response from client!!!!!" };
+        }       
+    }
+}
---
+++ k8.docker.app.server.user/Controllers/UserSrvController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace k8.docker.app.server.user.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSrvController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "!!!!!Response from server!!!!!" };
+        }       
+    }
+}
