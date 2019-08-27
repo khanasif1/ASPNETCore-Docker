67db70662fda6180e1c1362edea6ef477030fff7
Asif Khan <khanasif1@gmail.com>
2019-08-27 15:23:45 +1000

    Test checkin 1

--- k8.docker.app.server.user/Startup.cs
+++ k8.docker.app.server.user/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
-//Testing gitdfdfdff
+//Testing gitdfdfdffdfdf
 namespace k8.docker.app.server.user
 {
     public class Startup
